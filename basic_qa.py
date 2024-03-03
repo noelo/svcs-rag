362de5761b75a77353b52b82432f768920ca70b0
Noel O'Connor <noel.oconnor@gmail.com>
2024-03-03 13:37:50 +0000

    added FlagEmbeddingReranker

--- basic_qa.py
+++ basic_qa.py
@@ -8,6 +8,11 @@ import chainlit.data as cl_data
 from customchainlitds import TestDataLayer
 
 from llama_index.callbacks.global_handlers import set_global_handler
+from llama_index.callbacks import (
+    CallbackManager,
+    LlamaDebugHandler,
+    CBEventType,
+)
 
 from llama_index import (
     StorageContext,
@@ -45,18 +50,33 @@ from llama_index import QueryBundle
 from llama_index.retrievers import BaseRetriever
 from typing import Any, List
 from llama_index.schema import NodeWithScore
+from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
 
+import debugpy
+
+
+logging.basicConfig(level=logging.DEBUG)
+debugpy.listen(('0.0.0.0', 5678))
 
 set_global_handler("simple")
 
-logging.basicConfig(level=logging.DEBUG)
 
+logging.info("loading from ChromaDB")
 db2 = chromadb.PersistentClient(path="/var/home/noelo/dev/svcs-rag/chromadb",settings=Settings(anonymized_telemetry=False))
 chroma_collection = db2.get_or_create_collection("quickstart")
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
 index = VectorStoreIndex.from_vector_store(vector_store)
 docstore = SimpleDocumentStore.from_persist_path("/var/home/noelo/dev/svcs-rag/chromadb/docstore.json")
+logging.info("loading from ChromaDB....done")
+
+logging.info("loading from FlagEmbeddingReranker")
+reranker = FlagEmbeddingReranker(
+    top_n=10,
+    model="BAAI/bge-reranker-large",
+    use_fp16=False
+)
+logging.info("loading from FlagEmbeddingReranker....done")
 
 
 vector_retriever = VectorIndexRetriever(
@@ -68,31 +88,26 @@ vector_retriever = VectorIndexRetriever(
 
 bm25_retriever = BM25Retriever.from_defaults(
     docstore=docstore, 
-    similarity_top_k=2,
+    similarity_top_k=10,
     verbose=True
 )
 
-async def run_queries(queries, retrievers):
+def run_queries(query, retrievers):
     """Run queries against retrievers."""
-    tasks = []
-    for query in queries:
-        for i, retriever in enumerate(retrievers):
-            tasks.append(retriever.aretrieve(query))
 
-    task_results = await tqdm.gather(*tasks)
-
-    results_dict = {}
-    for i, (query, query_result) in enumerate(zip(queries, task_results)):
-        results_dict[(query, i)] = query_result
-
-    return results_dict
+    results_list= []
+    print("Query  ",query)
+    for i, retriever in enumerate(retrievers):
+            query_result = retriever.retrieve(query)
+            for value in query_result:
+                results_list.append(value)
+    return results_list
 
 def fuse_results(results_dict, similarity_top_k: int = 2):
     """Fuse results."""
     k = 60.0  # `k` is a parameter used to control the impact of outlier rankings.
     fused_scores = {}
     text_to_node = {}
-
     # compute reciprocal rank scores
     for nodes_with_scores in results_dict.values():
         for rank, node_with_score in enumerate(
@@ -117,6 +132,8 @@ def fuse_results(results_dict, similarity_top_k: int = 2):
         reranked_nodes.append(text_to_node[text])
         reranked_nodes[-1].score = score
 
+
+
     return reranked_nodes[:similarity_top_k]
 
 class FusionRetriever(BaseRetriever):
@@ -135,11 +152,12 @@ class FusionRetriever(BaseRetriever):
 
     def retrieve(query_bundle: QueryBundle) -> List[NodeWithScore]:
         """Retrieve."""
-        results_dict = run_queries(query_bundle.query_str, [vector_retriever, bm25_retriever])
-        final_results = fuse_results(
-            results_dict, similarity_top_k=2
-        )
-        return final_results
+        logging.info("Performing retrieve")
+        node_list = run_queries(query_bundle.query_str, [vector_retriever, bm25_retriever])
+
+        logging.info("Performing reranking")
+        ranked_nodes = reranker._postprocess_nodes(node_list, query_bundle)
+        return ranked_nodes
 
 
 qa_prompt = PromptTemplate(
@@ -172,7 +190,7 @@ Answer: \
 
 cl_data._data_layer = TestDataLayer()
 
-async def callLLM(query_engine:RetrieverQueryEngine,query:str):
+def callLLM(query_engine:RetrieverQueryEngine,query:str):
     # results_dict = await run_queries(query, [vector_retriever, bm25_retriever])
     # final_results = fuse_results(results_dict)
     # print(final_results)
@@ -182,13 +200,15 @@ async def callLLM(query_engine:RetrieverQueryEngine,query:str):
 
 @cl.on_chat_start
 async def factory():
+    llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+    # callback_manager = CallbackManager([llama_debug])
     service_context = ServiceContext.from_defaults(
-        callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]),
+        callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler(),llama_debug]),
     )
 
     # configure response synthesizer
     response_synthesizer = get_response_synthesizer(
-        text_qa_template=qa_prompt,
+        text_qa_template=qa_prompt,verbose=True
     )
 
     # assemble query engine
@@ -198,7 +218,7 @@ async def factory():
         retriever=FusionRetriever,
         service_context=service_context,
         response_synthesizer=response_synthesizer,
-        node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
+        # node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
         response_mode=ResponseMode.ACCUMULATE,
         verbose=True,
         streaming=True,
@@ -213,7 +233,7 @@ async def main(message: cl.Message):
     # response = await cl.make_async(query_engine.query)(message.content)
     # response = await cl.make_async(callLLM)(query_engine,message.content)
     
-    response = await callLLM(query_engine,message.content)
+    response = await cl.make_async(callLLM)(query_engine,message.content)
 
     response_message = cl.Message(content=f"{response}")
 
